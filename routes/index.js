01893f985a75a7c9e8d3eda0cda70bba13109485
Sujith Bheemireddi <sujithrb@Sujiths-MacBook-Pro.local>
2015-01-17 23:51:47 -0500

    Initial project setup

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express', firstName: 'Sujith' });
+});
+
+router.get('/name', function(req, res) {
+  res.render('layout', { });
+});
+
+module.exports = router;
