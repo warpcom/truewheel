8ebfa8f3345d40b587d11a082f5e05b7323832bc
Sujith Bheemireddi <sujithhgem@yahoo.com>
2015-01-20 22:16:05 -0500

    gulpfile.js file is cleaned

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,64 @@
 var gulp = require('gulp'),
 	scss = require('gulp-ruby-sass'),
 	server = require('gulp-develop-server'),
-	livereload = require('gulp-livereload');
+	livereload = require('gulp-livereload'),
+	Q = require('q');
 
-var options = {
-    path: 'bin/www'
-};
+var sassLocation = 'public/stylesheets/sass/**/*.scss',
+	cssLocation = 'public/stylesheets/',
+	applicationScript = 'app.js',
+	nodeRoutes = 'routes/**/*.js',
+	nodeViews = 'views/**/*.jade',
+	javascriptFiles = 'public/javascripts/**/*.js',
+	angularViews = 'public/views/**/*.html';
 
-gulp.task('server:start', function() {
-	server.listen(options, livereload.listen);
-});
+var nodeServer = {
+    	path: 'bin/www'
+	},
+	scssFileChangedPattern = /(.*\.scss$)/; // SASS files end with '.scss'
 
 gulp.task('styles', function() {
-	styleTask();
+	compileSass();
+});
+
+gulp.task('server:start', function() {
+	server.listen(nodeServer, livereload.listen);
 });
 
 gulp.task('watch', function() {
-	gulp.watch('sass/**/*.scss').on('change', function(file) {
-		styleTask();
-		restart(file);
-	});
-	gulp.watch(['app.js', 'routes/*.js', 'views/**/*.jade', 'public/javascripts/**/*.js', 'public/views/**/*.html']).on('change', restart);
+	gulp.watch([applicationScript, nodeRoutes, nodeViews, javascriptFiles, angularViews, sassLocation]).on('change', handleFileChanges);
 });
 
 gulp.task('default', ['styles', 'server:start', 'watch']);
 
-function styleTask() {
-	gulp.src('public/stylesheets/sass/**/*.scss')
-	.pipe(scss())
-	.pipe(gulp.dest('public/stylesheets/'));
+function compileSass() {
+	var deferred = Q.defer();
+	gulp.src(sassLocation)
+		.pipe(scss())
+		.pipe(gulp.dest(cssLocation))
+		.on('end', function() {
+			deferred.resolve();
+		});
+	return deferred.promise;
 };
 
-function restart( file ) {
-    server.changed( function( error ) {
-        if( ! error ) livereload.changed( file.path );
+function restartServer(file) {
+	/* Restart server. */
+    server.changed(function(error) { // It will throw exception if the server is not yet started [User Awareness].
+        if(!error) {
+        	livereload.changed(file.path);
+        } else {
+        	console.log('Error: ' + JSON.stringify(error));
+        }
     });
+}
+
+function handleFileChanges(file) {
+	/* Check if a SASS file has changed. */
+	if (scssFileChangedPattern.exec(file.path)) {
+		compileSass().then(restartServer(file));
+	} else {
+		restartServer(file);
+	}
+	
 };
